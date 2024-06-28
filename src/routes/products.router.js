de4ff730717f81b0dc97fbcbf88030efd83196bc
amantu07 <claudiom.garcia77@gmail.com>
2024-06-28 15:50:31 -0300

    Se refactorizó el código products.router para trabajar con una arquitectura por capas

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,107 +1,18 @@
 import { Router } from 'express'
-import ProductManager from '../dao/managers/mongomanagers/mongoProductManager.js'
-import { generateLink } from '../utils.js'
+import {
+    getProducts,
+    getProductById,
+    addProduct,
+    updateProduct,
+    deleteProduct
+} from '../controllers/products.controller.js'
 
 const productsRouter = Router()
 
-const productManager = new ProductManager()
-
-productsRouter.get('/', async (req, res) => {
-    try {
-        const { limit, sort, query, page } = req.query
-        const readProducts = await productManager.getProducts(limit, page, sort, query)
-
-        const { products, totalProducts, totalPages, currentPage } = readProducts
-
-        const hasPrevPage = currentPage > 1
-        const hasNextPage = currentPage < totalPages
-
-        res.json({
-            status: 'success',
-            payload: products,
-            totalPages,
-            prevPage: hasPrevPage ? currentPage - 1 : null,
-            nextPage: hasNextPage ? currentPage + 1 : null,
-            page: currentPage,
-            hasPrevPage,
-            hasNextPage,
-            prevLink: hasPrevPage ? generateLink('products', currentPage - 1, sort, limit, query) : null,
-            nextLink: hasNextPage ? generateLink('products', currentPage + 1, sort, limit, query) : null
-        })
-
-    } catch (error) {
-        res.status(500).json({ error: 'Hubo un error al obtener los productos.', message: error.message })
-    }
-})
-
-productsRouter.get('/:pid', async (req, res) => {
-    try {
-        const pid = req.params.pid
-        const product = await productManager.getProductById(pid)
-
-        if (!product) {
-            return res.status(404).json({ error: `No se encontró ningún producto con el id ${pid}.` })
-        }
-        res.json(product)
-    } catch (error) {
-        res.status(500).json({ error: 'Hubo un error al obtener el producto.', message: error.message })
-    }
-})
-
-productsRouter.post('/', async (req, res) => {
-    try {
-        const product = req.body
-        product.status = true
-
-        const createProduct = await productManager.addProduct(product)
-
-        if (createProduct === false) {
-            return res.status(400).json({ error: `Ya existe un producto con el código "${product.code}".` })
-        }
-
-        res.status(201).json({ message: `Se agregó correctamente el producto con el código "${product.code}".` })
-
-    } catch (error) {
-        res.status(500).json({ error: 'Hubo un error al agregar el producto.', message: error.message })
-    }
-})
-
-productsRouter.put('/:pid', async (req, res) => {
-    try {
-        const pid = req.params.pid
-        const fields = req.body
-
-        const product = await productManager.updateProduct(pid, fields)
-
-        if (product === false) {
-            return res.status(400).json({ error: `No se puede modificar el ID del producto` })
-        }
-
-        if (!product) {
-            return res.status(404).json({ error: `No se encontró ningún producto con el id ${pid}` })
-        }
-
-        res.json({ message: `Se actualizó el producto ${pid}` })
-
-    } catch (error) {
-        res.status(500).json({ error: 'Hubo un error al actualizar el producto.', message: error.message })
-    }
-})
-
-productsRouter.delete('/:pid', async (req, res) => {
-    try {
-        const pid = req.params.pid
-        const product = await productManager.deleteProduct(pid)
-
-        if (!product) {
-            return res.status(404).json({ error: `No se encontró ningún producto con el id ${pid}` })
-        }
-
-        res.json({ message: `Se eliminó correctamente el producto con el id ${pid}` })
-
-    } catch (error) {
-        res.status(500).json({ error: 'Hubo un error al intentar eliminar el producto.', message: error.message })
-    }
-})
+productsRouter.get('/', getProducts)
+productsRouter.get('/:pid', getProductById)
+productsRouter.post('/', addProduct)
+productsRouter.put('/:pid', updateProduct)
+productsRouter.delete('/:pid', deleteProduct)
 
 export default productsRouter
